2b8c2aa31061383717cb5d2be82e1e9a63d0575b
camilorojase <c.rojase@uniandes.edu.co>
2022-10-20 01:50:28 -0500

    changes to hello world

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 from flask import Flask
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello World"
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
-if _name_ == "_main_":
-	app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
 
